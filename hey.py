a16d70c47e718cada5d4970e84707490efee913f
bbqiu <bbqiu@anyscale.com>
2022-10-07 21:56:16 +0000

    1

--- hey.py
+++ hey.py
@@ -7,9 +7,8 @@ ray.init(address="auto")
 # 1: Define a Ray Serve deployment.
 @serve.deployment(route_prefix="/")
 class MyModelDeployment:
-    def __init__(self, msg: str):
-        # Initialize model state: could be very large neural net weights.
-        self._msg = msg
+    def __init__(self):
+        pass
 
     def __call__(self, request):
         return {"result": "Hey"}
@@ -19,7 +18,7 @@ class MyModelDeployment:
 serve.start(detached=True, http_options={"host": "0.0.0.0", "location": "EveryNode"})
 MyModelDeployment.deploy()
 
-# serve.run(MyModelDeployment.bind(msg="Hey!"))
+# serve.run(MyModelDeployment.bind(msg="Bye!"))
 
 # 3: Query the deployment and print the result.
 print(requests.get("http://localhost:8000/").json())
