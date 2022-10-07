fca30a061f8a0868f4eadee8f5a963fe835419d3
bbqiu <bbqiu@anyscale.com>
2022-10-07 21:47:50 +0000

    detach

--- bye.py
+++ bye.py
@@ -1,20 +1,24 @@
 from ray import serve
 import requests
+import ray
+
+ray.init(address="auto")
 
 # 1: Define a Ray Serve deployment.
 @serve.deployment(route_prefix="/")
 class MyModelDeployment:
-    def __init__(self, msg: str):
-        # Initialize model state: could be very large neural net weights.
-        self._msg = msg
+    def __init__(self):
+        pass
 
     def __call__(self, request):
         return {"result": "Bye"}
 
 
 # 2: Deploy the model.
-serve.start(http_options={"host": "0.0.0.0", "location": "EveryNode"})
-serve.run(MyModelDeployment.bind(msg="Bye!"))
+serve.start(detached=True, http_options={"host": "0.0.0.0", "location": "EveryNode"})
+MyModelDeployment.deploy()
+
+# serve.run(MyModelDeployment.bind(msg="Bye!"))
 
 # 3: Query the deployment and print the result.
 print(requests.get("http://localhost:8000/").json())
--- hey.py
+++ hey.py
@@ -1,5 +1,8 @@
 from ray import serve
 import requests
+import ray
+
+ray.init(address="auto")
 
 # 1: Define a Ray Serve deployment.
 @serve.deployment(route_prefix="/")
@@ -9,12 +12,14 @@ class MyModelDeployment:
         self._msg = msg
 
     def __call__(self, request):
-        return {"result": self._msg}
+        return {"result": "Hey"}
 
 
 # 2: Deploy the model.
-serve.start(http_options={"host": "0.0.0.0", "location": "EveryNode"})
-serve.run(MyModelDeployment.bind(msg="Hey!"))
+serve.start(detached=True, http_options={"host": "0.0.0.0", "location": "EveryNode"})
+MyModelDeployment.deploy()
+
+# serve.run(MyModelDeployment.bind(msg="Hey!"))
 
 # 3: Query the deployment and print the result.
 print(requests.get("http://localhost:8000/").json())
